50c749e1efb313724a5cdc2b38ed6aa68f65dd85
hsuh <mikepavelhsu@gmail.com>
2020-07-23 17:55:19 -0400

    Add Notes Repository and placeholder calls in usecase

--- app/src/main/java/com/example/newsFeed/mainFeed/domain/NotesUsecase.kt
+++ app/src/main/java/com/example/newsFeed/mainFeed/domain/NotesUsecase.kt
@@ -1,17 +1,21 @@
 package com.example.newsFeed.mainFeed.domain
 
 import com.example.newsFeed.mainFeed.data.Note
+import com.example.newsFeed.mainFeed.repository.NotesRepository
 
-class NoteUsecase {
+class NotesUsecase(val repository: NotesRepository) {
     // Retrieve all notes
     // TODO What would be the return type?
     // 1. suspend function to return a List<Note> data type
     // 2. a LiveDate<List<Note>>
     // TODO What would be the interaction between usecase and repository
     suspend fun retrieveNotes(): List<Note> {
-        return emptyList()
+        return repository.retrieveNotes()
     }
     // Insert one Note
+    suspend fun insertNote(note: Note) {
+        repository.insertNode()
+    }
 
     // Delete one Note
 
---
+++ app/src/main/java/com/example/newsFeed/mainFeed/repository/NotesRepository.kt
@@ -0,0 +1,13 @@
+package com.example.newsFeed.mainFeed.repository
+
+import com.example.newsFeed.mainFeed.data.Note
+
+class NotesRepository {
+    suspend fun retrieveNotes(): List<Note> {
+        return emptyList()
+    }
+
+    suspend fun insertNode() {
+
+    }
+}
