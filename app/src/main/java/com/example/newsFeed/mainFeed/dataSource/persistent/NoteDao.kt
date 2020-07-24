bb429281adabacc5ce7f7838b0a91ed3df36cc9a
hsuh <mikepavelhsu@gmail.com>
2020-07-24 00:23:49 -0400

    Add Dao for Note and add simple queries.

---
+++ app/src/main/java/com/example/newsFeed/mainFeed/dataSource/persistent/NoteDao.kt
@@ -0,0 +1,20 @@
+package com.example.newsFeed.mainFeed.dataSource.persistent
+
+import androidx.room.*
+import com.example.newsFeed.mainFeed.data.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNotes(vararg notes: Note)
+
+    @Update
+    fun updateNotes(vararg notes: Note)
+
+    @Delete
+    fun deleteNotes(vararg notes: Note)
+
+    @Query("SELECT * FROM note")
+    fun loadAllNotes(): Array<Note>
+}
