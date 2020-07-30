c847088f16d46ce7649392839a7983f2192a7f71
hsuh <mikepavelhsu@gmail.com>
2020-07-30 01:49:33 -0400

    Add instance variable to custom application class

--- app/src/main/java/com/example/newsFeed/NewsFeedApplication.kt
+++ app/src/main/java/com/example/newsFeed/NewsFeedApplication.kt
@@ -1,9 +1,25 @@
 package com.example.newsFeed
 
 import android.app.Application
+import com.example.newsFeed.di.AppComponent
+import com.example.newsFeed.di.AppModule
+import com.example.newsFeed.di.DaggerAppComponent
 
 class NewsFeedApplication : Application() {
+
+    companion object {
+        lateinit var instance: NewsFeedApplication
+    }
+
+
+    lateinit var appComponent: AppComponent
+
     override fun onCreate() {
         super.onCreate()
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+
+        instance = this
     }
 }
