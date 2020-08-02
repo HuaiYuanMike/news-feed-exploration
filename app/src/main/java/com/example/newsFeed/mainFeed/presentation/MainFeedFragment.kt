00141959a9f0abafdcd556e69f421902e0f345d8
hsuh <mikepavelhsu@gmail.com>
2020-08-02 00:23:30 -0400

    Set support action bar. Init MainFeedFragment layout.

--- app/src/main/java/com/example/newsFeed/mainFeed/presentation/MainFeedFragment.kt
+++ app/src/main/java/com/example/newsFeed/mainFeed/presentation/MainFeedFragment.kt
@@ -1,4 +1,24 @@
 package com.example.newsFeed.mainFeed.presentation
 
-class MainFeedFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.newsFeed.R
+
+class MainFeedFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main_feed, container, false )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as AppCompatActivity).setSupportActionBar(view?.findViewById(R.id.tool_bar))
+    }
 }
