ac78aae4edaa0f25d4590628abe8e77401d3b397
hsuh <mikepavelhsu@gmail.com>
2020-08-15 01:41:40 -0400

    Add internal LiveData object with backing property. Add placeholder Kotlin coroutine launcher for MainFeedViewModel.

--- app/src/main/java/com/example/newsFeed/mainFeed/presentation/MainFeedViewModel.kt
+++ app/src/main/java/com/example/newsFeed/mainFeed/presentation/MainFeedViewModel.kt
@@ -4,10 +4,33 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.newsFeed.mainFeed.data.Note
+import com.example.newsFeed.mainFeed.domain.NoteUseCase
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainFeedViewModel : ViewModel() {
+// TODO Dependency injection
+class MainFeedViewModel constructor(private val noteUseCase: NoteUseCase) : ViewModel() {
 
-    private val innerLiveData: MutableLiveData<List<Note>> = MutableLiveData<List<Note>>()
+    private val _notes: MutableLiveData<List<Note>> = MutableLiveData<List<Note>>()
 
-    val notes: LiveData<List<Note>> = innerLiveData
+    val notes: LiveData<List<Note>> = _notes
+
+    // This does not need to be suspend
+    fun getAllNotes() = GlobalScope.launch {
+        val deferred = GlobalScope.async {
+            noteUseCase.retrieveAllNotes()
+        }
+        _notes.postValue(deferred.await())
+    }
 }
+// TODO List to confirm
+// 1. Kotlin coroutine launchers - launch(), async()
+// 2. ViewModel kotlin ktx byViewModels() method
+// 3. Dependency injection or something else which remove the usage of ViewModelProvider and Factory
+// 4. ViewModel SavedStateHandle
+// 5. HILT
+// 6. Retrofit - REST API - HTTP ?
+// 7. Image loading library
