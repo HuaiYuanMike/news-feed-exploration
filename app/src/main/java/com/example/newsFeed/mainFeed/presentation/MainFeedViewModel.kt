5864e797f80ef16592aa61fd7622d97cde0fd4a6
hsuh <mikepavelhsu@gmail.com>
2020-08-17 01:11:57 -0400

    Simplify the coroutine launcher in MainFeedViewModel.

--- app/src/main/java/com/example/newsFeed/mainFeed/presentation/MainFeedViewModel.kt
+++ app/src/main/java/com/example/newsFeed/mainFeed/presentation/MainFeedViewModel.kt
@@ -5,13 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.newsFeed.mainFeed.data.Note
 import com.example.newsFeed.mainFeed.domain.NoteUseCase
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import javax.inject.Inject
+import kotlinx.coroutines.*
 
-// TODO Dependency injection
 class MainFeedViewModel constructor(private val noteUseCase: NoteUseCase) : ViewModel() {
 
     private val _notes: MutableLiveData<List<Note>> = MutableLiveData<List<Note>>()
@@ -19,15 +14,14 @@ class MainFeedViewModel constructor(private val noteUseCase: NoteUseCase) : View
     val notes: LiveData<List<Note>> = _notes
 
     // This does not need to be suspend
-    fun getAllNotes() = GlobalScope.launch {
-        val deferred = GlobalScope.async {
-            noteUseCase.retrieveAllNotes()
-        }
-        _notes.postValue(deferred.await())
+    fun getAllNotes() = GlobalScope.launch(Dispatchers.Default) {
+        _notes.postValue(noteUseCase.retrieveAllNotes())
     }
 }
 // TODO List to confirm
-// 1. Kotlin coroutine launchers - launch(), async()
+// 1. Kotlin coroutine launchers - launch(), async() - and withContext()
+//      Kotlin running multiple coroutines concurrently
+// 1.1 Kotlin CoroutineScope - ViewModel scope
 // 2. ViewModel kotlin ktx byViewModels() method
 // 3. Dependency injection or something else which remove the usage of ViewModelProvider and Factory
 // 4. ViewModel SavedStateHandle
