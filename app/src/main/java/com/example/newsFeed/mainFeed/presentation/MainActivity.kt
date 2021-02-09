2f95149e446110da728c2dc0e9bef73296fb957e
hsuh <mikepavelhsu@gmail.com>
2021-02-09 01:19:03 -0500

    Add Tabs to the swipeable pages.

--- app/src/main/java/com/example/newsFeed/mainFeed/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsFeed/mainFeed/presentation/MainActivity.kt
@@ -7,19 +7,29 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.example.newsFeed.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager: ViewPager2
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewPager = findViewById(R.id.viewpager)
         viewPager.adapter = MainViewPagerAdapter(this)
+
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        TabLayoutMediator(tabLayout, viewPager,
+            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                tab.text = "Title $position"
+            }
+        ).attach()
     }
 
-    private class MainViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    private class MainViewPagerAdapter(fragmentActivity: FragmentActivity) :
+        FragmentStateAdapter(fragmentActivity) {
         override fun getItemCount() = 3
 
         override fun createFragment(position: Int): Fragment {
